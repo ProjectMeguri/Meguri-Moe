3fddf66eb45d858eea2197940b4901baa9b70fab
Zhaowei Zhong <zhong98zhaowei@gmail.com>
2024-03-21 02:07:32 +0900

    Fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import type { DocumentProps } from 'next/document'
-import i18nextConfig from '../next-i18next.config.js'
-import Script from 'next/script'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import type { DocumentProps } from 'next/document';
+import i18nextConfig from '../next-i18next.config.js';
+import Script from 'next/script';
 
 type Props = DocumentProps & {
   // add custom document props
-}
+};
 
 class MyDocument extends Document<Props> {
   render() {
-    const currentLocale = this.props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale
+    const currentLocale =
+      this.props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
     return (
       <Html lang={currentLocale}>
         <Head>
-          <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@500;700&display=swap' />
+          <link
+            rel="stylesheet"
+            href="https://static.miracdn.com/fonts/index.css"
+          />
         </Head>
         <body>
-          <Script src="https://www.googletagmanager.com/gtag/js?id=G-Y6BGS89WT5" strategy="afterInteractive" />
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-Y6BGS89WT5"
+            strategy="afterInteractive"
+          />
           <Script id="google-analytics" strategy="afterInteractive">
             {`
             window.dataLayer = window.dataLayer || [];
@@ -30,9 +37,8 @@ class MyDocument extends Document<Props> {
           <NextScript />
         </body>
       </Html>
-    )
-
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
